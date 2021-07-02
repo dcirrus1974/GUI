fdbf46a2d2d287b6dcf587c6e307ef7e24073594
markojovanovic410 <markojovfreelancer@gmail.com>
2021-07-02 07:45:03 +0700

    sixth commit from yev

--- src/pages/file_manager/Personal_Files/index.tsx
+++ src/pages/file_manager/Personal_Files/index.tsx
@@ -1,6 +1,11 @@
 import React, {useState, useEffect, useRef} from 'react'
 import Menubar from '../../../components/MenuBar'
 import Header from '../../../layouts/Header'
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import ViewAgendaIcon from '@material-ui/icons/ViewAgenda';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+
 import './index.scss';
 const Personal_Files: React.FC = () => {
     return (
@@ -9,8 +14,14 @@ const Personal_Files: React.FC = () => {
         <div className="personal_page">
           <Menubar/>
           <div className="personal_page_right">
-            <div className="personal_title">PERSONAL FILES</div>
-                        
+            <div className="personal_page_right_top">
+              <div className="personal_title">PERSONAL FILES</div>
+              <div className="personal_btn_group">
+                <Button variant="contained" color="primary">New Folder<AddIcon/></Button>
+                <Button variant="contained" color="primary">Upload&nbsp;<CloudUploadIcon/></Button>
+                <div className="icon_side"><ViewAgendaIcon/></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>      
